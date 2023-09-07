6c096c054bb12b88cbc9acc0e2119b75ba68ede7
Coy McNew <mcnewcp@gmail.com>
2023-09-07 11:00:29 -0500

    removed api key

--- streamlit_app_v2.py
+++ streamlit_app_v2.py
@@ -8,15 +8,14 @@ st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 # Replicate Credentials
 with st.sidebar:
     st.title("🦙💬 Llama 2 Chatbot")
-    if "REPLICATE_API_TOKEN" in st.secrets:
-        st.success("API key already provided!", icon="✅")
-        replicate_api = st.secrets["REPLICATE_API_TOKEN"]
+    # if "REPLICATE_API_TOKEN" in st.secrets:
+    #     st.success("API key provided!", icon="✅")
+    #     replicate_api = st.secrets["REPLICATE_API_TOKEN"]
+    replicate_api = st.text_input("Enter Replicate API token:", type="password")
+    if not (replicate_api.startswith("r8_") and len(replicate_api) == 40):
+        st.warning("Please enter your credentials!", icon="⚠️")
     else:
-        replicate_api = st.text_input("Enter Replicate API token:", type="password")
-        if not (replicate_api.startswith("r8_") and len(replicate_api) == 40):
-            st.warning("Please enter your credentials!", icon="⚠️")
-        else:
-            st.success("Proceed to entering your prompt message!", icon="👉")
+        st.success("Proceed to entering your prompt message!", icon="👉")
 
     # Refactored from https://github.com/a16z-infra/llama2-chatbot
     st.subheader("Models and parameters")
@@ -44,7 +43,7 @@ with st.sidebar:
     )
 
     st.markdown(
-        ":computer: Code available [here](https://github.com/Overtrained/contextual-qa-chat-app)"
+        ":computer: [repo](https://github.com/Overtrained/contextual-qa-chat-app)"
     )
 
 os.environ["REPLICATE_API_TOKEN"] = replicate_api
